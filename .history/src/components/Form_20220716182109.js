8b871eeb06819aa20b39a3bdac636db8738b0723
peterclermy <peterclermy232@gmail.com>
2022-07-16 18:36:55 +0300

    created a form

---
+++ .history/src/components/Form_20220716182109.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+
+stName] = useState("John");
+Name] = useState("Henry");
+Change(event) {
+get.value);
+
+hange(event) {
+et.value);
+
+
+
+nChange={handleFirstNameChange} value={firstName} />
+nChange={handleLastNameChange} value={lastName} />
+">Submit</button>
+
+
+
+
+export default Form;
---
+++ .history/src/components/Form_20220716182118.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+
+
+
+
+
+
+
+
+
+">Submit</button>
+
+
+
+
+export default Form;
